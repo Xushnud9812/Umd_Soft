1ba52a6f1d9ba93a945d47ba1abf3f5c5023a77b
Zafar6699 <webzafar6699@gmail.com>
2021-04-21 11:12:01 +0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="test"></div>
+    <button class="btn btn-success">OK</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+div.test {
+  height: 50px;
+  width: 100%;
+  background-color: $gc;
+}
+</style>
